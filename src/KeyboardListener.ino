1fe0cb4cafd36d911fba127018a7bb930677d9a0
BuildTools <unconfigured@null.spigotmc.org>
2024-01-23 18:38:59 -0500

    Implemented Some Buttons

--- src/KeyboardListener.ino
+++
@@ -1,9 +0,0 @@
-void setup() {
-  Serial.begin(9600); // Choose the appropriate baud rate
-}
-
-void loop() {
-  // Your code here
-  Serial.println("Hello, World!"); // Example output
-  delay(3000);
-}
---
+++ src/KeyboardListener/KeyboardListener.ino
@@ -0,0 +1,76 @@
+int ButtonState2 = 0;
+int lastButtonState2 = 0;
+int ButtonState3 = 0;
+int lastButtonState3 = 0;
+int ButtonState4 = 0;
+int lastButtonState4 = 0;
+int ButtonState5 = 0;
+int lastButtonState5 = 0;
+
+const int aButton = 2;
+const int bButton = 3;
+const int spaceButton = 5;
+const int nukeButton = 4;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(aButton, INPUT);
+  pinMode(bButton, INPUT);
+  pinMode(spaceButton, INPUT);
+  pinMode(nukeButton, INPUT);
+}
+
+void loop(){
+  ButtonState2 = digitalRead(aButton);
+  if(ButtonState2 != lastButtonState2)
+  {
+    if(ButtonState2 == LOW)
+    {
+      Serial.println('A');
+    }
+  }
+  lastButtonState2 = ButtonState2;
+
+  ButtonState3 = digitalRead(bButton);
+  if(ButtonState3 != lastButtonState3)
+  {
+    if(ButtonState3 == LOW)
+    {
+      Serial.println('B');
+    }
+  }
+  lastButtonState3 = ButtonState3; 
+
+  ButtonState4 = digitalRead(nukeButton);
+  if(ButtonState4 != lastButtonState4)
+  {
+    if(ButtonState4 == LOW)
+    {
+      Serial.println(' ');
+      Serial.println("LAUNCHING NUKE");
+      delay(1000);
+      Serial.println('5');
+      delay(1000);
+      Serial.println('4');
+      delay(1000);
+      Serial.println('3');
+      delay(1000);
+      Serial.println('2');
+      delay(1000);
+      Serial.println('1');
+      delay(100);
+      Serial.println("BOOM");
+      Serial.println(' ');
+    }
+  }
+  lastButtonState4 = ButtonState4;
+
+  ButtonState5 = digitalRead(spaceButton);
+  if (ButtonState5 == lastButtonState5)
+  {
+    if (ButtonState5 == LOW)
+    {
+      Serial.println(' ');
+    }
+  }
+}
