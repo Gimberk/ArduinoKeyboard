e3797765ff55cd4c2c622278b1647651b79bd4d7
BuildTools <unconfigured@null.spigotmc.org>
2024-01-23 19:25:33 -0500

    Key Simulations

--- src/SerialListener.py
+++ src/SerialListener.py
@@ -1,9 +1,12 @@
 import serial
+from pynput.keyboard import Key, Controller
 
-port = "COM4"
+keyboard = Controller()
+
+port = "COM6"
 baudRate = 9600
 
 serial = serial.Serial(port, baudRate)
 while True:
-    line = serial.readline().decode('utf-8')
-    print(line, end='')
+    line = serial.readline().decode('utf-8').rstrip('\n')
+    keyboard.press(str(line[:1]))
